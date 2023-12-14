b66e292caf4c0fbe0b8a8cb3e0b572848e6b323f
J-Groesbeck <jacob.groesbeck@icloud.com>
2023-12-14 14:56:09 -0500

    Added big portion of tutorial JS with my edits

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function shakeMagic8Ball() {
-    
+    const question = prompt('Ask the Magic 8-Ball a question:');
+    if (question === null) {
+        document.getElementById('response-text').innerText = 'I said ASK ME A QUESTION'
+        document.getElementById('response-text').classList = 'text-danger display-4'
+        document.getElementById('response-text').src = ''
+    } else if (!question.trim()) {
+        alert('Please enter a valid question!')
+        return
+    } else {
+        document.getElementById('response-text').innerText = `Your Question Was: ${question}`
+        document.getElementById('response-text').classList = 'display-4'
+    }
 }
