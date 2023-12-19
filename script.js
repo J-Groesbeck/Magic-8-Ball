56edd59adb4e26269e89568df8dce6abd36fa76a
J-Groesbeck <jacob.groesbeck@icloud.com>
2023-12-19 15:00:10 -0500

    idk what i did tbh

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ function shakeMagic8Ball() {
     if (question === null) {
         document.getElementById('response-text').innerText = 'I said ASK ME A QUESTION'
         document.getElementById('response-text-2').innerText = ''
-        document.getElementById('response-text').classList = 'text-danger display-4'
+        document.getElementById('response-text').classList = 'text-danger'
         document.getElementById('response-img').src = 'img/HEYSTOPTHAT.png'
+        document.getElementById('response-img').classList = 'mb-5'
     } else if (!question.trim()) {
         alert('Please enter a valid question!')
         return
     } else {
         document.getElementById('response-text').innerText = `Your Question Was: ${question}`
-        document.getElementById('response-text').classList = 'display-4'
+        document.getElementById('response-text').classList = ''
         document.getElementById('response-img').src = 'img/Magic-8-Ball.png'
         giveResult();
     }
@@ -22,35 +23,35 @@ function giveResult() {
     switch (randomNumber) {
         case 0:
             responseText.innerText = 'Yes'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 1:
             responseText.innerText = 'answer2'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 2:
             responseText.innerText = 'answer3'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 3:
             responseText.innerText = 'answer4'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 4:
             responseText.innerText = 'answer5'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 5:
             responseText.innerText = 'answer6'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 6:
             responseText.innerText = 'answer7'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
         case 7:
             responseText.innerText = 'answer8'
-            responseText.classList = 'display-4 text-success'
+            responseText.classList = 'text-success'
             break;
     }
 }
